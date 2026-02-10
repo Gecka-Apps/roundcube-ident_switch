da573c184e4d1e1b145a98689ba1352e92e9f27f
Laurent Dinclaux <laurent@gecka.nc>
2026-02-10 22:30:20 +1100

    Merge parent_id migration into single 2026021000 upgrade script

--- SQL/mysql/2026021000.sql
+++ SQL/mysql/2026021000.sql
@@ -74,3 +74,13 @@ ALTER TABLE `ident_switch`
 		smallint
 		DEFAULT NULL
 		AFTER `notify_sound`;
+
+-- Add alias support: parent_id links an alias identity to a parent account
+ALTER TABLE `ident_switch`
+	ADD `parent_id`
+		int UNSIGNED
+		DEFAULT NULL
+		AFTER `iid`;
+
+ALTER TABLE `ident_switch`
+	ADD INDEX `IX_ident_switch_parent_id` (`parent_id`);
--- SQL/postgres/2026021000.sql
+++ SQL/postgres/2026021000.sql
@@ -51,3 +51,9 @@ ALTER TABLE ident_switch
 	ADD notify_desktop
 		smallint
 		DEFAULT NULL;
+
+-- Add alias support: parent_id links an alias identity to a parent account
+ALTER TABLE ident_switch
+	ADD parent_id integer DEFAULT NULL;
+
+CREATE INDEX IX_ident_switch_parent_id ON ident_switch(parent_id);
